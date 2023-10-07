adbcba7d67239b88860d550fb9ee940a8d07141e
Imran Khan <imran-k@outlook.com>
2023-10-07 11:50:07 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 namespace csharp2022
 {
@@ -6,7 +8,17 @@ namespace csharp2022
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // String value = "imran khan";
+           // Console.WriteLine($"My name is {value}");
+
+           int fahrenheit = 94;
+           decimal tempincelsius = (fahrenheit - 32)* 5 / 9m;
+      //     Console.Write("The temperature is ");
+     //      Console.Write(tempincelsius);
+     //      Console.Write(5/10);
+     Console.WriteLine("Windows " + 7 + 4);
+     Console.WriteLine("Windows " + 11);
+     Console.Write("Windows " + 1 + 1);
         }
     }
 }
